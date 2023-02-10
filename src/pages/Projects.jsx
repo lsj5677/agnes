c094badf3af3d3f94f8d44f178771e2fc773711a
lsj5677 <lsj5677@naver.com>
2023-02-10 02:37:08 -0500

    netlify push

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 export default function Projects() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    setLoading(true);
-    setError(undefined);
     fetch("/data/projects.json")
       .then((res) => res.json())
       .then((data) => setProjects(data))
-      .catch((error) => console.debug("Error", error))
-      .finally(() => setLoading(false));
+      .catch((error) => console.debug("Error", error));
   }, []);
   return (
     <div className="sub-wrap sub-layout sm:grid grid-flow-row-dense grid-cols-2 gap-5 lg:grid-cols-3 xl:grid-cols-4">
