4d1755360db393b41e8167ffa28d0340152a8ed0
lsj5677 <lsj5677@naver.com>
2023-04-06 22:23:02 -0400

    Update Card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const BUTTON_STYLE =
-  "inline-block py-2 px-6 border-2 border-black text-center rounded-md hover:bg-red ease-in-out duration-200 dark:text-black";
+  "inline-block py-2 px-6 border-1 border-black text-center rounded-sm hover:bg-red ease-in-out duration-200 dark:text-black";
 
 export default function Card({
   title,
@@ -16,8 +16,8 @@ export default function Card({
   const [imageLoading, setImageLoading] = useState(true);
 
   return (
-    <div className="relative bg-white mb-10 mx-auto rounded-md max-w-sm shadow-[1px_-3px_14px_rgba(0,0,0,0.3)] dark:shadow-[2px_-3px_14px_rgba(249,248,241,0.7)] dark:bg-black">
-      <div className="rounded-md overflow-hidden bg-lightGray opacity-70 min-h-[30%]">
+    <div className="relative bg-white mb-10 mx-auto rounded-md max-w-md shadow-[1px_-3px_14px_rgba(0,0,0,0.3)] dark:shadow-[2px_-3px_14px_rgba(249,248,241,0.7)] dark:bg-black">
+      <div className="rounded-md overflow-hidden bg-lightGray min-h-[30%]">
         <motion.img
           src={thumbSrc}
           alt={title}
@@ -30,22 +30,7 @@ export default function Card({
         />
       </div>
       <div className="p-5">
-        <div className="flex items-center justify-between">
-          <h3 className="text-xl font-bold">{title}</h3>
-          <span className="text-xs opacity-40">{period}</span>
-        </div>
-        <p className="my-2">{desc}</p>
-        <div>
-          {tags.map((tag, id) => (
-            <span
-              key={id}
-              className="inline-block text-xs bg-lightGray mr-1.5 px-1.5 rounded-sm dark:text-black"
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
-        <div className="flex items-center justify-center gap-4 mt-5 text-black absolute top-16 left-1/2 -translate-x-1/2">
+        <div className="flex items-center justify-center gap-4 my-5 text-black ">
           <a
             href={gitSrc}
             target="_blank"
@@ -62,6 +47,21 @@ export default function Card({
           >
             website
           </a>
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl font-bold">{title}</h3>
+          <span className="text-xs opacity-40">{period}</span>
+        </div>
+        <p className="my-2">{desc}</p>
+        <div>
+          {tags.map((tag, id) => (
+            <span
+              key={id}
+              className="inline-block text-xs bg-lightGray mr-1.5 px-1.5 rounded-sm dark:text-black"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
       </div>
     </div>
